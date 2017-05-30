0bcce8784def023aa11f5aba2fe01f502bea7813
sisuicheng <sisuicheng@yeepay.com>
2017-05-30 13:28:36 +0800

    增加版本version

--- checkauth-core/src/main/java/com/check/auth/g3/checkauth/dao/IAuthInstDirDao.java
+++ checkauth-core/src/main/java/com/check/auth/g3/checkauth/dao/IAuthInstDirDao.java
@@ -1,7 +1,7 @@
 package com.check.auth.g3.checkauth.dao;
 
 
-import com.check.entity.AuthInstDirEntity;
+import com.check.auth.g3.checkauth.entity.AuthInstDirEntity;
 
 public interface IAuthInstDirDao {
 	public void insertAuthInstDir(AuthInstDirEntity authInstDirEntity);

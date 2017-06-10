20fd4b33467c8e085278f356f751248fe92958f1
dengyj3 <dengyj3@139.com>
2017-06-10 16:05:37 +0800

    commit 详情表实体

--- checkauth-core/src/main/java/com/check/auth/g3/core/checkauth/entity/AuthInstDetailEntity.java
+++ checkauth-core/src/main/java/com/check/auth/g3/core/checkauth/entity/AuthInstDetailEntity.java
@@ -1,49 +1,42 @@
 package com.check.auth.g3.core.checkauth.entity;
 
 public class AuthInstDetailEntity extends AuthInstEntity {
-	private String address;//北京市丰台区南四环西路188号9区
 	private String busiScope;//认证类别代码
 	private String busiScopeName;//认证类别及认证领域
 	private String businessNumber;//业务代码
 	private String cccBusinessScope;//CCC指定业务范围
-	private String contactPeroson;//联系人
-	private String districtCode;//区号
-	private String districtCodeName;//所在地区
-	private String email;//email
+	private String oldInstCode;//原机构批准号
 	private String exchangeDate;//换发日期, eg.2016-09-24
 	private String fax;//传真
-	private int id;//id
-	private String instCode;//机构批准号
-	private String instName;//机构名称
-	private String instStatus;//机构状态
-	private String instType;//机构类别
-	private String oldInstCode;//原机构批准号
-	private String orgCode;//统一社会信用代码/组织机构代码
+	private String email;//email
 	private String post;//邮编
-	private String principal;//法定代表人
 	private String principalRegNum;//法人注册号
 	private String principalTypeName;//法人类型, eg.事业单位
-	private String ratifyDate;//颁发日期,eg. 2002-12-10
 	private String recordScope;//登记范围
 	private String recordTs;//登记日期,eg.2017-03-09 16:38:05',
 	private String regCapital;//eg. 18000
 	private String regCapitalType;//01
-	private String remarks;//备注, eg.经批准的其他认证：分包IQ Net成员认证机构的管理体系认证业务
-	private String tel;//联系电话, eg.010-83886666
-	private String validityDate;//有效截止日期
 	private String website;//网址, eg.www.cqc.com.cn
+	private String isCCInst;//是否为3C认证机构,0代表否,1代表是
 	private String zdjgBm;//eg.01
 	private String zdjgName;//中国质量认证中心
-	
 	private String cnasRec;//CNAS认可
 	private String outsideRec;//境外认可
 	private String highnewTecRec;//高新技术认可
+	private String orgCode;//统一社会信用代码/组织机构代码
+	private String entHode;//企业控股
 	
-	public String getAddress() {
-		return address;
+	public String getIsCCInst() {
+		return isCCInst;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+	public void setIsCCInst(String isCCInst) {
+		this.isCCInst = isCCInst;
+	}
+	public String getEntHode() {
+		return entHode;
+	}
+	public void setEntHode(String entHode) {
+		this.entHode = entHode;
 	}
 	public String getBusiScope() {
 		return busiScope;
@@ -69,24 +62,6 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	public void setCccBusinessScope(String cccBusinessScope) {
 		this.cccBusinessScope = cccBusinessScope;
 	}
-	public String getContactPeroson() {
-		return contactPeroson;
-	}
-	public void setContactPeroson(String contactPeroson) {
-		this.contactPeroson = contactPeroson;
-	}
-	public String getDistrictCode() {
-		return districtCode;
-	}
-	public void setDistrictCode(String districtCode) {
-		this.districtCode = districtCode;
-	}
-	public String getDistrictCodeName() {
-		return districtCodeName;
-	}
-	public void setDistrictCodeName(String districtCodeName) {
-		this.districtCodeName = districtCodeName;
-	}
 	public String getEmail() {
 		return email;
 	}
@@ -104,36 +79,6 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	}
 	public void setFax(String fax) {
 		this.fax = fax;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getInstCode() {
-		return instCode;
-	}
-	public void setInstCode(String instCode) {
-		this.instCode = instCode;
-	}
-	public String getInstName() {
-		return instName;
-	}
-	public void setInstName(String instName) {
-		this.instName = instName;
-	}
-	public String getInstStatus() {
-		return instStatus;
-	}
-	public void setInstStatus(String instStatus) {
-		this.instStatus = instStatus;
-	}
-	public String getInstType() {
-		return instType;
-	}
-	public void setInstType(String instType) {
-		this.instType = instType;
 	}
 	public String getOldInstCode() {
 		return oldInstCode;
@@ -153,12 +98,6 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	public void setPost(String post) {
 		this.post = post;
 	}
-	public String getPrincipal() {
-		return principal;
-	}
-	public void setPrincipal(String principal) {
-		this.principal = principal;
-	}
 	public String getPrincipalRegNum() {
 		return principalRegNum;
 	}
@@ -170,12 +109,6 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	}
 	public void setPrincipalTypeName(String principalTypeName) {
 		this.principalTypeName = principalTypeName;
-	}
-	public String getRatifyDate() {
-		return ratifyDate;
-	}
-	public void setRatifyDate(String ratifyDate) {
-		this.ratifyDate = ratifyDate;
 	}
 	public String getRecordScope() {
 		return recordScope;
@@ -200,24 +133,6 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	}
 	public void setRegCapitalType(String regCapitalType) {
 		this.regCapitalType = regCapitalType;
-	}
-	public String getRemarks() {
-		return remarks;
-	}
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getValidityDate() {
-		return validityDate;
-	}
-	public void setValidityDate(String validityDate) {
-		this.validityDate = validityDate;
 	}
 	public String getWebsite() {
 		return website;
@@ -258,18 +173,14 @@ public class AuthInstDetailEntity extends AuthInstEntity {
 	}
 	@Override
 	public String toString() {
-		return "AuthInstMainInfoEntity [address=" + address + ", busiScope=" + busiScope + ", busiScopeName="
-				+ busiScopeName + ", businessNumber=" + businessNumber + ", cccBusinessScope=" + cccBusinessScope
-				+ ", contactPeroson=" + contactPeroson + ", districtCode=" + districtCode + ", districtCodeName="
-				+ districtCodeName + ", email=" + email + ", exchangeDate=" + exchangeDate + ", fax=" + fax + ", id="
-				+ id + ", instCode=" + instCode + ", instName=" + instName + ", instStatus=" + instStatus
-				+ ", instType=" + instType + ", oldInstCode=" + oldInstCode + ", orgCode=" + orgCode + ", post=" + post
-				+ ", principal=" + principal + ", principalRegNum=" + principalRegNum + ", principalTypeName="
-				+ principalTypeName + ", ratifyDate=" + ratifyDate + ", recordScope=" + recordScope + ", recordTs="
-				+ recordTs + ", regCapital=" + regCapital + ", regCapitalType=" + regCapitalType + ", remarks="
-				+ remarks + ", tel=" + tel + ", validityDate=" + validityDate + ", website=" + website + ", zdjgBm="
-				+ zdjgBm + ", zdjgName=" + zdjgName + ", cnasRec=" + cnasRec + ", outsideRec=" + outsideRec
-				+ ", highnewTecRec=" + highnewTecRec + "]";
+		return "AuthInstDetailEntity [busiScope=" + busiScope + ", busiScopeName=" + busiScopeName + ", businessNumber="
+				+ businessNumber + ", cccBusinessScope=" + cccBusinessScope + ", oldInstCode=" + oldInstCode
+				+ ", exchangeDate=" + exchangeDate + ", fax=" + fax + ", email=" + email + ", post=" + post
+				+ ", principalRegNum=" + principalRegNum + ", principalTypeName=" + principalTypeName + ", recordScope="
+				+ recordScope + ", recordTs=" + recordTs + ", regCapital=" + regCapital + ", regCapitalType="
+				+ regCapitalType + ", website=" + website + ", isCCInst=" + isCCInst + ", zdjgBm=" + zdjgBm
+				+ ", zdjgName=" + zdjgName + ", cnasRec=" + cnasRec + ", outsideRec=" + outsideRec + ", highnewTecRec="
+				+ highnewTecRec + ", orgCode=" + orgCode + ", entHode=" + entHode + "]";
 	}
 	
 	
